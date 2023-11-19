85fba0a61c8abd5322053c781ae3418b6c3a587e
rencon0417 <yugo04172930@icloud.com>
2023-11-19 15:31:33 +0900

    gate

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -30,6 +30,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'role' => 9,   //（１：開発者、５：管理者、９：一般ユーザー）
         ]);
     }
 }
